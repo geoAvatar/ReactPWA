14315b9080f0ab3bb95eafa4b73e3488d7cb53f3
Esseddik ismael <ismael.esseddik@gmail.com>
2018-10-20 18:17:43 +0200

    new app w/ CRA

---
+++ memory/src/Test.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+class Test extends Component {
+    render(){
+        return (
+            <div className="Test">
+            <body>
+                <p>
+                    je test
+                </p>
+            </body>
+
+            </div>
+        );
+    }
+}
+
+export default Test;
