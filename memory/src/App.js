513677952e02a5d13bfe874252da799755b5cb46
Esseddik ismael <ismael.esseddik@gmail.com>
2018-10-23 17:25:53 +0200

    memory tutorial progress

--- memory/src/App.js
+++ memory/src/App.js
@@ -1,30 +1,56 @@
-
 import React, { Component } from 'react'
+import shuffle from 'lodash.shuffle'
+
 import './App.css'
 
 import Card from './Card'
 import GuessCount from './GuessCount'
-import Greeter from './Greeter'
+import HallOfFame,{FAKE_HOF} from './HallOfFame'
+
+const SIDE = 6
+const SYMBOLS = '😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="memory">
-        <GuessCount guesses={0} />
-        <Card card="😀" feedback="hidden" onClick={this.handleCardClick}/>
-        <Card card="🎉" feedback="justMatched" onClick={this.handleCardClick} />
-        <Card card="💖" feedback="justMismatched" onClick={this.handleCardClick} />
-        <Card card="🎩" feedback="visible" onClick={this.handleCardClick} />
-        <Card card="🐶" feedback="hidden" onClick={this.handleCardClick} />
-        <Card card="🐱" feedback="justMatched" onClick={this.handleCardClick} />
-        <Greeter whom="you" />
-      </div>
-        
-    )
+  state = {
+    cards: this.generateCards(),
+    currentPair: [],
+    guesses: 0,
+    matchedCardIndices: [],
+  }
+
+  generateCards() {
+    const result = []
+    const size = SIDE * SIDE
+    const candidates = shuffle(SYMBOLS)
+    while (result.length < size) {
+      const card = candidates.pop()
+      result.push(card, card)
+    }
+    return shuffle(result)
   }
 
   handleCardClick(card) {
     console.log(card, 'clicked')
+  }
+
+  render() {
+    const won = new Date().getSeconds() % 2 === 0
+    return (
+      <div className="memory">
+        <GuessCount guesses={0} />
+        <Card card="😀" feedback="hidden" onClick={this.handleCardClick} />
+        <Card card="🎉" feedback="justMatched" onClick={this.handleCardClick} />
+        <Card
+          card="💖"
+          feedback="justMismatched"
+          onClick={this.handleCardClick}
+        />
+        <Card card="🎩" feedback="visible" onClick={this.handleCardClick} />
+        <Card card="🐶" feedback="hidden" onClick={this.handleCardClick} />
+        <Card card="🐱" feedback="justMatched" onClick={this.handleCardClick} />
+        {won && <HallOfFame entries={FAKE_HOF} />}
+      </div>
+    )
   }
 }
 
--- memory/src/GuessCount.js
+++ memory/src/GuessCount.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 import './GuessCount.css'
 
+import PropTypes from 'prop-types'
+
+
 const GuessCount = () => <div className="guesses" />
+
+GuessCount.propTypes = {
+    guesses: PropTypes.number.isRequired,
+}
 
 export default GuessCount
